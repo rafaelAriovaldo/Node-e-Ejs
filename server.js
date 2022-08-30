a21f89fb8bccca346e5ff717f00fc22935b6857c
RafaelAriovaldo <rafaelariovaldo02@gmail.com>
2022-08-30 16:39:44 -0300

    Update-code the end.

--- server.js
+++ server.js
@@ -3,13 +3,43 @@ const app = express();
 
 app.set("view engine", "ejs");
 
-app.get("/", function(req, res){
-   res.render("pages/index");
+app.get("/", function (req, res) {
+   const itens = [
+      {
+         Title: "D",
+         message: "esenvolver aplicações de forma fácil"
+      },
+      {
+         Title: "E",
+         message: "js usa Java script para redenrizar Html"
+      },
+      {
+         Title: "M",
+         message: "uito facil de Usar"
+      },
+      {
+         Title: "A",
+         message: "lefante"
+      },
+      {
+         Title: "I",
+         message: "nstall Ejs"
+      },
+      {
+         Title: "S",
+         message: "intaxe simples."
+      }
+   ];
+   const subtitle = "Uma linguagem de modelagem para criação de pagina html utilzando JS"
+   res.render("pages/index",{
+      qualitys: itens,
+      subtitle: subtitle,
+   });
 })
 
-app.get("/sobre", function(req, res){
-    res.render("pages/about");
- })
+app.get("/sobre", function (req, res) {
+   res.render("pages/about");
+})
 
- app.listen(8080);
- console.log("Run...")
+app.listen(8080);
+console.log("Run...")
